86b555e087a6a3f2b55fdd3174be2d1bfc608038
ronsavage <ron@savage.net.au>
2016-09-05 08:07:52 +1000

    Patch Makefile.PL and the docs

--- lib/HTML/Entities/Interpolate.pm
+++ lib/HTML/Entities/Interpolate.pm
@@ -96,6 +96,9 @@ David Nicol <davidnicol@gmail.com>.
 
 Maintainer: Ron Savage I<E<lt>ron@savage.net.auE<gt>>.
 
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
 The full text of the license can be found in the
 LICENSE file included with this module.
 
